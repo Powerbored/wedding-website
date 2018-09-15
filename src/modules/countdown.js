f3ad686c64e256181261d13b59634e8f8c356019
Powerbored <andrew.daniel.lewis@gmail.com>
2018-09-15 22:33:28 +1000

    Complete rolling countdown timer

--- src/modules/countdown.js
+++ src/modules/countdown.js
@@ -22,8 +22,8 @@ export function init(targetElement, endTimeString) {
 		// 	seconds: a * 60,
 		// 	years: 0
 		// }, values.time);
-		updateElement(targetElement, values.difference, values.time);
-	}, 100);
+		updateElement(targetElement, values.difference, values.time, window.matchMedia('(prefers-reduced-motion)').matches);
+	}, Math.floor(1000/30));
 	// function step() {
 	// 	updateElement(targetElement, endTimeString);
 	// 	window.requestAnimationFrame(step);
@@ -40,18 +40,14 @@ function evaluateDifferenceFromNow(endTimeString) {
 	return {time, endTime, difference};
 }
 
-function updateElement(targetElement, diff, time) {
+function updateElement(targetElement, diff, time, reduceMotion) {
 	targetElement.innerHTML = `
+		${formatMonthsDeciaml(diff, time)}
+		${formatDaysDeciaml(diff, time)}
+		${formatHoursDeciaml(diff, time)}
+		${formatMinutesDeciaml(diff, time)}
 		${formatSecondsDeciaml(diff, time)}
 	`;
-	// ${formatMonths(difference, currentTime, padAndSplitNumber(difference.months)[0], padAndSplitNumber())}
-	// ${formatMonths(difference, currentTime, padAndSplitNumber(difference.months)[1], padAndSplitNumber())}
-	// ${formatDays(difference, currentTime, padAndSplitNumber(difference.days)[0], padAndSplitNumber())}
-	// ${formatDays(difference, currentTime, padAndSplitNumber(difference.days)[1], padAndSplitNumber())}
-	// ${formatHours(difference, currentTime, padAndSplitNumber(difference.hours)[0], padAndSplitNumber(difference.time.clone().add(25, 'days').daysInMonth()[0]))}
-	// ${formatHours(difference, currentTime, padAndSplitNumber(difference.hours)[1], padAndSplitNumber(difference.time.clone().add(25, 'days').daysInMonth())[1])}
-	// ${formatMinutes(difference, currentTime, padAndSplitNumber(difference.minutes)[0], padAndSplitNumber())}
-	// ${formatMinutes(difference, currentTime, padAndSplitNumber(difference.minutes)[1], padAndSplitNumber())}
 }
 
 function padAndSplitNumber(number) {
@@ -62,10 +58,10 @@ function padAndSplitNumber(number) {
 function formatValuesAsHtml(classList, value, remainder, nextMax) {
 	return `
 		<div class="${classList}">
-			<span style="transform: rotateX(${((value >= 0 ? remainder : 0) - 1) * -36}deg) translateZ(4.8rem)">
+			<span style="transform: rotateX(${(1 - (value >= 0 ? remainder : 0)) * 36}deg) translateZ(3.4rem)">
 				${value}
 			</span>
-			<span style="transform: rotateX(${(value >= 0 ? remainder : 0) * -36}deg) translateZ(4.8rem)" role="presentation">
+			<span style="transform: rotateX(${(value >= 0 ? remainder : 0) * -36}deg) translateZ(3.4rem); background: hsl(16, 5%, ${(1 - remainder) * 30 + 50}%)" role="presentation">
 				${value - 1 >= 0 ? value - 1 : nextMax}
 			</span>
 		</div>
@@ -90,10 +86,111 @@ function formatSecondsTens(diff, time, value, ones, maxValue) {
 }
 function formatSecondsDeciaml(diff, time) {
 	const
-		nextMax = 59;
+		nextMax = 60,
+		remainder = 1 - time.milliseconds()/1000,
+		formatTensParams = [
+			'seconds tens',
+			tens(diff.seconds),
+			remainder * 0.1 + ones(diff.seconds)/10,
+			tens(nextMax - 1),
+		],
+		formatOnesParams = [
+			'seconds ones',
+			ones(diff.seconds),
+			remainder,
+			ones(nextMax - 1),
+		];
 	return `
-		${formatSecondsTens(diff, time, tens(diff.seconds), ones(diff.seconds), tens(nextMax))}
-		${formatSecondsOnes(diff, time, ones(diff.seconds), ones(nextMax))}
+		${formatValuesAsHtml(...formatTensParams)}
+		${formatValuesAsHtml(...formatOnesParams)}
+	`;
+}
+
+function formatMinutesDeciaml(diff, time) {
+	const
+		nextMax = 60,
+		remainder = diff.seconds/60 - time.milliseconds()/(1000 * 60),
+		formatTensParams = [
+			'minutes tens',
+			tens(diff.minutes),
+			remainder * 0.1 + ones(diff.minutes)/10,
+			tens(nextMax - 1),
+		],
+		formatOnesParams = [
+			'minutes ones',
+			ones(diff.minutes),
+			remainder,
+			ones(nextMax - 1),
+		];
+	return `
+		${formatValuesAsHtml(...formatTensParams)}
+		${formatValuesAsHtml(...formatOnesParams)}
+	`;
+}
+
+function formatHoursDeciaml(diff, time) {
+	const
+		nextMax = 24,
+		remainder = diff.minutes/60 + diff.seconds/(60 * 60),
+		formatTensParams = [
+			'hours tens',
+			tens(diff.hours),
+			remainder * 0.1 + ones(diff.hours - 1)/10,
+			tens(nextMax),
+		],
+		formatOnesParams = [
+			'hours ones',
+			ones(diff.hours),
+			remainder,
+			ones(nextMax - 1),
+		];
+	return `
+		${formatValuesAsHtml(...formatTensParams)}
+		${formatValuesAsHtml(...formatOnesParams)}
+	`;
+}
+
+function formatDaysDeciaml(diff, time) {
+	const
+		nextMax = time.clone().add(diff.days + 15, 'days').daysInMonth(),
+		remainder = diff.hours/24 + diff.minutes/(60 * 24),
+		formatTensParams = [
+			'days tens',
+			tens(diff.days),
+			remainder * 0.1 + ones(diff.days)/10,
+			tens(nextMax - 1),
+		],
+		formatOnesParams = [
+			'days ones',
+			ones(diff.days),
+			remainder,
+			ones(nextMax - 1),
+		];
+	return `
+		${formatValuesAsHtml(...formatTensParams)}
+		${formatValuesAsHtml(...formatOnesParams)}
+	`;
+}
+
+function formatMonthsDeciaml(diff, time) {
+	const
+		nextMax = 12,
+		remainder = diff.days/time.daysInMonth() + diff.hours/(24 * time.daysInMonth()),
+		formatTensParams = [
+			'months tens',
+			tens(diff.months),
+			remainder * 0.1 + ones(diff.months)/10,
+			tens(nextMax - 1),
+		],
+		formatOnesParams = [
+			'months ones',
+			ones(diff.months),
+			remainder,
+			ones(nextMax - 1),
+		];
+	return `
+		${formatValuesAsHtml(...formatTensParams)}
+		${formatValuesAsHtml(...formatOnesParams)}
 	`;
 }
 
