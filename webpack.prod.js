fa3daee55a7af2302842f807195734ba99b8b26e
Powerbored <andrew.daniel.lewis@gmail.com>
2018-09-26 09:41:13 +1000

    Run new build accounting for CNAME

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,14 @@ const
 	path = require('path'),
 	merge = require('webpack-merge'),
 	common = require('./webpack.common.js'),
+	CopyWebpackPlugin = require('copy-webpack-plugin'),
 	CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
 	plugins: [
 		new CleanWebpackPlugin([path.resolve(__dirname, 'docs/*')]),
+		new CopyWebpackPlugin([{
+			from: 'CNAME',
+		}]),
 	],
 });
